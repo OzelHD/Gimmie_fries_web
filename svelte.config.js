0c63a5c0263668d0c26ddedaa1e81ad6cbdd4e0f
OzelHD <davide0929carocci@gmail.com>
2025-04-15 23:47:01 +0200

    new path for base

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-  kit: {
-    adapter: adapter({
-      strict: false
-    })
-  }
+const dev = process.env.NODE_ENV === 'development';
+
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/Gimmie_fries_web'
+		},
+		prerender: {
+			default: true
+		}
+	}
 };
+
+export default config;
